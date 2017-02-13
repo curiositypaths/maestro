13e4658df6a9f35d173b976d2a6b9b092af2d8ca
curiositypaths <curiositypaths@users.noreply.github.com>
2017-02-13 13:37:12 -0500

    Cleanup project template

--- src/App.js
+++ src/App.js
@@ -6,12 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="">
+          <h2>Welcome to Maestro</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Learning trails about programming, design and&hellip;
         </p>
       </div>
     );
