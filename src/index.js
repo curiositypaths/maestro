208b0d429c5690d3a99b3b6b9f593ba21ce136d5
curiositypaths <curiositypaths@users.noreply.github.com>
2017-02-13 13:56:27 -0500

    Add basic directory structure for react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+import './routes.js';
 import App from './App';
 import './index.css';
 
