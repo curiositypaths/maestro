9f90b6af8a1e6f9a5b10484366256a372b98baf4
curiositypaths <curiositypaths@users.noreply.github.com>
2017-02-17 15:17:59 -0500

    Remove unused import

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 //import { fetchUser } from '../actions/index'
-import { authUser, logOutUser, fetchUser } from '../actions/index'
+import { authUser, logOutUser } from '../actions/index'
 
 class NavBar extends Component {
 	constructor(props) {
