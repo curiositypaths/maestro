6b25d43e421ef69b94b02f2ff5e298e7c6ef84c5
curiositypaths <curiositypaths@users.noreply.github.com>
2017-02-22 14:11:12 -0500

    Update New Trail and Profile links

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -44,7 +44,7 @@ class NavBar extends Component {
 	render() {
 		let logInAndOutOutOptions = ''
     if (this.currentUserIsSet() === true) {
-      logInAndOutOutOptions = <div className="navbar__buttons__container"><a href={`/trails/new`}>Post a new trail</a><a href={`/users/${this.props.users.currentUser.id}`}>Profile</a><a href="" onClick={ this.handleLogout }>Logout</a></div>
+      logInAndOutOutOptions = <div className="navbar__buttons__container"><Link to={'/trails/new'}>Post a new trail</Link><Link to={`/users/${this.props.users.currentUser.id}`}>Profile</Link><a href="" onClick={ this.handleLogout }>Logout</a></div>
     } else {
       logInAndOutOutOptions = <div className="navbar__buttons__container"><Link to={'/login'}>Login</Link><Link to={'/register'}>Register</Link></div>
     }
